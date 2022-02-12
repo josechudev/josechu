d1514e256388cd03e205c59d9d3d4f4ac1b75a46
jchudb93 <jchudb93@gmail.com>
2022-02-12 18:40:32 -0500

    Timeline and accomplishments

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -7,45 +7,12 @@ import { TimeLineData } from '../../constants/constants';
 const TOTAL_CAROUSEL_COUNT = TimeLineData.length;
 
 const Timeline = () => {
-  // const [activeItem, setActiveItem] = useState(0);
-  // const carouselRef = useRef();
-
-  // const scroll = (node, left) => {
-  //   return node.scrollTo({ left, behavior: 'smooth' });
-  // }
-
-  // const handleClick = (e, i) => {
-  //   e.preventDefault();
-
-  //   if (carouselRef.current) {
-  //     const scrollLeft = Math.floor(carouselRef.current.scrollWidth * 0.7 * (i / TimeLineData.length));
-      
-  //     scroll(carouselRef.current, scrollLeft);
-  //   }
-  // }
-
-  // const handleScroll = () => {
-  //   if (carouselRef.current) {
-  //     const index = Math.round((carouselRef.current.scrollLeft / (carouselRef.current.scrollWidth * 0.7)) * TimeLineData.length);
-
-  //     setActiveItem(index);
-  //   }
-  // }
-
-  // // snap back to beginning of scroll when window is resized
-  // // avoids a bug where content is covered up if coming from smaller screen
-  // useEffect(() => {
-  //   const handleResize = () => {
-  //     scroll(carouselRef.current, 0);
-  //   }
-
-  //   window.addEventListener('resize', handleResize);
-  // }, []);
-
   return (
-    <div>
-      Timeline
-    </div>
+    <Section>
+      <SectionDivider></SectionDivider>
+      <SectionTitle>About Me</SectionTitle>
+      <SectionText>I am a Machine Learning Engineer</SectionText>
+    </Section>
   );
 };
 
