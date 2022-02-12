42d81a252c63278e5f9f7d2033f2b14b4ea6ef1a
jchudb93 <jchudb93@gmail.com>
2022-02-11 19:14:52 -0500

    Initial commit

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,7 +6,6 @@ import { projects } from '../../constants/constants';
 
 const Projects = () => {
 
-  
   return (<Section nopadding id='projects'>
   <SectionDivider />
   <SectionTitle></SectionTitle>
